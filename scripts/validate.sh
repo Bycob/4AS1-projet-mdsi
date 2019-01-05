4ccd5562966fe2589fcc4779588879538453d231
BynaryCobweb <ljean@etud.insa-toulouse.fr>
2019-01-05 15:12:49 +0100

    personnel.xml validates

---
+++ scripts/validate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# go to the root of the project
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $current_dir/..
+
+# validate xml
+echo -e "\e[44mValidating personnel.xml...\e[49m"
+xmllint --schema schema/personnel.xsd examples/personnel.xml
+echo -e "\e[44mValidating ufs.xml...\e[49m"
+xmllint --schema schema/uf.xsd examples/ufs.xml
+echo -e "\e[44mValidating maquette.xml...\e[49m"
+xmllint --schema schema/maquette.xsd examples/maquette.xml
